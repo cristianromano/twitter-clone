9ec781fd3701b8f4873e730f15292e4f6cfd0fcf
cristianromano <63170214+cristianromano@users.noreply.github.com>
2024-12-18 22:14:05 -0300

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
 import "./App.css";
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage.jsx";
+import LoginPage from "./pages/auth/LoginPage.jsx";
+import SignUpPage from "./pages/auth/SignUpPage.jsx";
+import Sidebar from "./components/common/Sidebar.jsx";
+import RightPanel from "./components/common/RightPanel.jsx";
+import NotificationPage from "./pages/notifications/NotificationPage.jsx";
+import ProfilePage from "./pages/profile/ProfilePage.jsx";
 function App() {
   return (
     <>
-      <p className="text">Hello</p>
+      <div className="flex max-w-6xl mx-auto">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/notifications" element={<NotificationPage />} />
+          <Route path="/profile/:username" element={<ProfilePage />} />
+        </Routes>
+        <RightPanel />
+      </div>
     </>
   );
 }
