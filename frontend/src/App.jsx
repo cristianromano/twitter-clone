bfc15bf1976fa4039edfc9a0da4b60d6d3c6b931
cristianromano <63170214+cristianromano@users.noreply.github.com>
2024-12-19 23:09:24 -0300

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import Sidebar from "./components/common/Sidebar.jsx";
 import RightPanel from "./components/common/RightPanel.jsx";
 import NotificationPage from "./pages/notifications/NotificationPage.jsx";
 import ProfilePage from "./pages/profile/ProfilePage.jsx";
+import { Toaster } from "react-hot-toast";
+
 function App() {
   return (
     <>
@@ -20,6 +22,7 @@ function App() {
           <Route path="/profile/:username" element={<ProfilePage />} />
         </Routes>
         <RightPanel />
+        <Toaster />
       </div>
     </>
   );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000", // Your back-end URL
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
